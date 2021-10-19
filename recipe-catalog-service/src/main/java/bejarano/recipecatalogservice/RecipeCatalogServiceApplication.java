b3a95dffa795aae8f84c09875515d4f0ef9df084
Jose Garcia <josefbgarcia3@gmail.com>
2021-10-19 09:32:55 +0300

    Adding Catalog RestController

--- recipe-catalog-service/src/main/java/bejarano/recipecatalogservice/RecipeCatalogServiceApplication.java
+++ recipe-catalog-service/src/main/java/bejarano/recipecatalogservice/RecipeCatalogServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipeCatalogServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RecipeCatalogServiceApplication.class, args);
 	}
 
