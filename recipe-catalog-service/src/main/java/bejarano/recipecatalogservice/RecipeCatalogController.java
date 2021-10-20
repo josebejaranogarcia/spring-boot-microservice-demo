f875e4db5a4a22ed3599b4c57f34c556b1f5d0dc
Jose Garcia <josefbgarcia3@gmail.com>
2021-10-20 13:01:04 +0300

    optimizing imports

--- recipe-catalog-service/src/main/java/bejarano/recipecatalogservice/RecipeCatalogController.java
+++ recipe-catalog-service/src/main/java/bejarano/recipecatalogservice/RecipeCatalogController.java
@@ -1,9 +1,7 @@
 package bejarano.recipecatalogservice;
 
 
-import bejarano.recipecatalogservice.models.Rating;
 import bejarano.recipecatalogservice.models.Recipe;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
--- recipe-info-service/src/main/java/bejarano/recipedetailsservice/RecipeDetailsController.java
+++ recipe-info-service/src/main/java/bejarano/recipedetailsservice/RecipeDetailsController.java
@@ -1,7 +1,10 @@
 package bejarano.recipedetailsservice;
 
 import bejarano.recipedetailsservice.models.RecipeDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
